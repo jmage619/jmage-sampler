62cc516f0d7deddf35540f18a3788e5ee30877a8
Jeff <jmage619@gmail.com>
2017-07-14 22:45:09 -0700

    implement add zone

--- jmage-sampler/jmage-sampler.cpp
+++ jmage-sampler/jmage-sampler.cpp
@@ -1,24 +1,111 @@
 #include <iostream>
-#include <jack/types.h>
-#include <jack/jack.h>
+#include <vector>
+#include <map>
+
+#include <cstring>
+#include <cstdlib>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-#include <cstring>
+#include <jack/types.h>
+#include <jack/jack.h>
 
 #include <config.h>
+#include <lib/jmsampler.h>
+#include <lib/wave.h>
+#include <lib/zone.h>
 
 typedef jack_default_audio_sample_t sample_t;
 
-struct jack_sampler {
+struct client_data {
   jack_port_t* output_port1;
   jack_port_t* output_port2;
+  std::map<std::string, jm::wave> waves;
+  std::vector<jm::zone> zones;
+  int zone_number;
+  JMSampler* sampler;
 };
 
+void build_zone_str(char* outstr, int len, client_data* cli_data, int i) {
+  memset(outstr, 0, len);
+  char* p = outstr;
+  sprintf(p, "add_zone:");
+  // index
+  p += strlen(p);
+  sprintf(p, "%i,", i);
+  // wave length
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].wave_length);
+  // name
+  p += strlen(p);
+  sprintf(p, "%s,", cli_data->zones[i].name);
+  std::cerr << "UI: adding ui zone!! " << cli_data->zones[i].name << std::endl;
+  // amp
+  p += strlen(p);
+  sprintf(p, "%f,", cli_data->zones[i].amp);
+  // origin
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].origin);
+  // low key
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].low_key);
+  // high key
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].high_key);
+  // low vel
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].low_vel);
+  // high vel
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].high_vel);
+  // pitch
+  p += strlen(p);
+  sprintf(p, "%f,", cli_data->zones[i].pitch_corr);
+  // start
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].start);
+  // left
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].left);
+  // right
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].right);
+  // loop mode
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].loop_mode);
+  // crossfade
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].crossfade);
+  // group
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].group);
+  // off group
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].off_group);
+  // attack
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].attack);
+  // hold
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].hold);
+  // decay
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].decay);
+  // sustain
+  p += strlen(p);
+  sprintf(p, "%f,", cli_data->zones[i].sustain);
+  // release
+  p += strlen(p);
+  sprintf(p, "%i,", cli_data->zones[i].release);
+  // path
+  p += strlen(p);
+  sprintf(p, "%s\n", cli_data->zones[i].path);
+}
+
 int process_callback(jack_nframes_t nframes, void* arg) {
-  jack_sampler* sampler = (jack_sampler*) arg;
-  sample_t* buffer1 = (sample_t*) jack_port_get_buffer(sampler->output_port1, nframes);
-  sample_t* buffer2 = (sample_t*) jack_port_get_buffer(sampler->output_port2, nframes);
+  client_data* cli_data = static_cast<client_data*>(arg);
+  sample_t* buffer1 = (sample_t*) jack_port_get_buffer(cli_data->output_port1, nframes);
+  sample_t* buffer2 = (sample_t*) jack_port_get_buffer(cli_data->output_port2, nframes);
   memset(buffer1, 0, sizeof(sample_t) * nframes);
   memset(buffer2, 0, sizeof(sample_t) * nframes);
   return 0;
@@ -34,17 +121,27 @@ int main() {
     return 1;
   }
 
-  jack_sampler* sampler = new jack_sampler;
+  client_data* cli_data = new client_data;
 
-  jack_set_process_callback(client, process_callback, sampler);
-  sampler->output_port1 = jack_port_register(client, "out1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-  sampler->output_port2 = jack_port_register(client, "out2", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  jack_set_process_callback(client, process_callback, cli_data);
+  cli_data->output_port1 = jack_port_register(client, "out1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  cli_data->output_port2 = jack_port_register(client, "out2", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+
+  cli_data->zones.reserve(100);
+
+  jack_nframes_t sample_rate = jack_get_sample_rate(client);
+  // supposed to also implement jack_set_buffer_size_callback; for now assume rarely changes
+  jack_nframes_t jack_buf_size = jack_get_buffer_size(client);
+  cli_data->sampler = new JMSampler(cli_data->zones, sample_rate, jack_buf_size, jack_buf_size);
+
+  cli_data->zone_number = 1;
 
   if (jack_activate(client)) {
-    jack_port_unregister(client, sampler->output_port1);
-    jack_port_unregister(client, sampler->output_port2);
+    jack_port_unregister(client, cli_data->output_port1);
+    jack_port_unregister(client, cli_data->output_port2);
     jack_client_close(client);
-    delete sampler;
+    delete cli_data->sampler;
+    delete cli_data;
     std::cerr <<"cannot activate jack client" << std::endl;
     return 1;
   }
@@ -71,28 +168,69 @@ int main() {
   close(from_child_pipe[1]);
   close(to_child_pipe[0]);
 
-  // close fd's subsequent children don't need on exec
-  fcntl(from_child_pipe[0], F_SETFD, FD_CLOEXEC); 
-  // this fd is important to close on exec so child properly gets EOF when parent closes fout
-  fcntl(to_child_pipe[1], F_SETFD, FD_CLOEXEC); 
-
   FILE* fin = fdopen(from_child_pipe[0], "r");
   FILE* fout = fdopen(to_child_pipe[1], "w");
 
   char buf[256];
 
+  fprintf(fout, "update_vol:16\n");
+  fflush(fout);
+
   while (fgets(buf, 256, fin) != NULL) {
     std::cout << "UI: " << buf;
+    if (!strncmp(buf, "add_zone:", 9)) {
+      char* p = strtok(buf + 9, ",");
+      int index = atoi(p);
+      p = strtok(NULL, ",");
+
+      p[strlen(p) - 1] = '\0';
+
+      if (cli_data->waves.find(p) == cli_data->waves.end()) {
+        cli_data->waves[p] = jm::parse_wave(p);
+      }
+
+      jm::wave& wav = cli_data->waves[p];
+      jm::zone zone;
+      jm::init_zone(&zone);
+      zone.wave = wav.wave;
+      zone.num_channels = wav.num_channels;
+      zone.sample_rate = wav.sample_rate;
+      zone.wave_length = wav.length;
+      zone.left = wav.left;
+      zone.right = wav.length;
+      if (wav.has_loop)
+        zone.loop_mode = jm::LOOP_CONTINUOUS;
+      sprintf(zone.name, "Zone %i", cli_data->zone_number++);
+      strcpy(zone.path, p);
+
+      char outstr[256];
+      if (index >= 0) {
+        cli_data->zones.insert(cli_data->zones.begin() + index, zone);
+        build_zone_str(outstr, 256, cli_data, index);
+      }
+      else {
+        cli_data->zones.push_back(zone);
+        build_zone_str(outstr, 256, cli_data, cli_data->zones.size() - 1);
+      }
+      fprintf(fout, outstr);
+      fflush(fout);
+    }
   }
 
   fclose(fout);
   waitpid(pid, NULL, 0);
 
   jack_deactivate(client);
-  jack_port_unregister(client, sampler->output_port1);
-  jack_port_unregister(client, sampler->output_port2);
+  jack_port_unregister(client, cli_data->output_port1);
+  jack_port_unregister(client, cli_data->output_port2);
   jack_client_close(client);
-  delete sampler;
+
+  std::map<std::string, jm::wave>::iterator it;
+  for (it = cli_data->waves.begin(); it != cli_data->waves.end(); ++it)
+    jm::free_wave(it->second);
+
+  delete cli_data->sampler;
+  delete cli_data;
 
   return 0;
 }
