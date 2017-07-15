b143e1114d1e8dbbf1773e51e63299f2449ce7a9
Jeff <jmage619@gmail.com>
2017-07-15 13:30:38 -0700

    begin implementing midi controls

--- jmage-sampler/jmage-sampler.cpp
+++ jmage-sampler/jmage-sampler.cpp
@@ -4,11 +4,13 @@
 
 #include <cstring>
 #include <cstdlib>
+#include <cstdio>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <jack/types.h>
 #include <jack/jack.h>
+#include <jack/midiport.h>
 
 #include <config.h>
 #include <lib/jmsampler.h>
@@ -18,11 +20,13 @@
 typedef jack_default_audio_sample_t sample_t;
 
 struct client_data {
+  jack_port_t* input_port;
   jack_port_t* output_port1;
   jack_port_t* output_port2;
   std::map<std::string, jm::wave> waves;
   std::vector<jm::zone> zones;
   int zone_number;
+  int channel;
   JMSampler* sampler;
 };
 
@@ -108,6 +112,40 @@ int process_callback(jack_nframes_t nframes, void* arg) {
   sample_t* buffer2 = (sample_t*) jack_port_get_buffer(cli_data->output_port2, nframes);
   memset(buffer1, 0, sizeof(sample_t) * nframes);
   memset(buffer2, 0, sizeof(sample_t) * nframes);
+
+  cli_data->sampler->pre_process(nframes);
+
+  // capture midi event
+  void* midi_buf = jack_port_get_buffer(cli_data->input_port, nframes);
+
+  uint32_t event_count = jack_midi_get_event_count(midi_buf);
+  uint32_t cur_event = 0;
+  jack_midi_event_t event;
+  if (event_count > 0)
+    jack_midi_event_get(&event, midi_buf, cur_event);
+
+  // loop over frames in this callback window
+  for (jack_nframes_t n = 0; n < nframes; ++n) {
+    if (cur_event < event_count) {
+      // procces next midi event if it applies to current time (frame)
+      while (n == event.time) {
+        // only consider events from current channel
+        if ((event.buffer[0] & 0x0f) == cli_data->channel) {
+          // process note on
+          if ((event.buffer[0] & 0xf0) == 0x90) {
+            cli_data->sampler->handle_note_on(event.buffer, nframes, n);
+          }
+        }
+        // get next midi event or break if none left
+        ++cur_event;
+        if (cur_event == event_count)
+          break;
+        jack_midi_event_get(&event, midi_buf, cur_event);
+      }
+    }
+    cli_data->sampler->process_frame(n, 1.0, buffer1, buffer2);
+  }
+
   return 0;
 }
 
@@ -124,6 +162,7 @@ int main() {
   client_data* cli_data = new client_data;
 
   jack_set_process_callback(client, process_callback, cli_data);
+  cli_data->input_port = jack_port_register(client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
   cli_data->output_port1 = jack_port_register(client, "out1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
   cli_data->output_port2 = jack_port_register(client, "out2", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 
@@ -135,8 +174,10 @@ int main() {
   cli_data->sampler = new JMSampler(cli_data->zones, sample_rate, jack_buf_size, jack_buf_size);
 
   cli_data->zone_number = 1;
+  cli_data->channel = 0;
 
   if (jack_activate(client)) {
+    jack_port_unregister(client, cli_data->input_port);
     jack_port_unregister(client, cli_data->output_port1);
     jack_port_unregister(client, cli_data->output_port2);
     jack_client_close(client);
@@ -216,6 +257,7 @@ int main() {
   waitpid(pid, NULL, 0);
 
   jack_deactivate(client);
+  jack_port_unregister(client, cli_data->input_port);
   jack_port_unregister(client, cli_data->output_port1);
   jack_port_unregister(client, cli_data->output_port2);
   jack_client_close(client);
