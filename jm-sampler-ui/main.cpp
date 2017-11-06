60eb062e269614293e62cc492a87e0c67998d19d
Jeff <jmage619@gmail.com>
2017-11-06 12:02:55 -0800

    allow ui to pass title through command args

--- jm-sampler-ui/main.cpp
+++ jm-sampler-ui/main.cpp
@@ -8,6 +8,11 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   SamplerUI sampler;
+  if (argc > 1)
+    sampler.setWindowTitle(argv[1]);
+  else
+    sampler.setWindowTitle("JMAGE Sampler");
+
   sampler.show();
   return app.exec();
 }
